3c661a34061bcb0fac2e1ae540a271e988cb59b5
DucHM <haminhducyka@gmail.com>
2026-01-24 13:53:11 +0700

    v1.4

--- script.js
+++ script.js
@@ -517,6 +517,21 @@ function renderTodayLunar() {
     `Âm lịch: ${lunar.lunarDay} tháng ${lunar.lunarMonth} năm ${canChiYear}`;
 }
 
+function updateClock() {
+  const now = new Date();
+
+  const h = String(now.getHours()).padStart(2, "0");
+  const m = String(now.getMinutes()).padStart(2, "0");
+  const s = String(now.getSeconds()).padStart(2, "0");
+
+  document.getElementById("clock").innerText = `${h}:${m}:${s}`;
+}
+
+// cập nhật mỗi giây
+setInterval(updateClock, 1000);
+updateClock();
+
+
 /* ========================== INIT ========================= */
 renderCalendar();
 renderToday();
